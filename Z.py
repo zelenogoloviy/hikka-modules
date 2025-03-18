44776a198006ec499cc5493c90edea91b26b9696
zelenogoloviy <zelenogoloviy@gmail.com>
2025-03-18 22:48:31 +0300

    Add files via upload

---
+++ Z.py
@@ -0,0 +1,70 @@
+# the source code was taken from software "H", available via link: https://mods.codwiz.life/H.py. original source code is subject to a proprietary license. which you can read at the link: https://github.com/C0dwiz/H.Modules?tab=readme-ov-file#-license
+# Proprietary License Agreement
+
+# Copyright (c) 2024-29 CodWiz
+
+# Permission is hereby granted to any person obtaining a copy of this software and associated documentation files (the "Software"), to use the Software for personal and non-commercial purposes, subject to the following conditions:
+
+# 1. The Software may not be modified, altered, or otherwise changed in any way without the explicit written permission of the author.
+
+# 2. Redistribution of the Software, in original or modified form, is strictly prohibited without the explicit written permission of the author.
+
+# 3. The Software is provided "as is", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event shall the author or copyright holder be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the Software or the use or other dealings in the Software.
+
+# 4. Any use of the Software must include the above copyright notice and this permission notice in all copies or substantial portions of the Software.
+
+# 5. By using the Software, you agree to be bound by the terms and conditions of this license.
+
+# For any inquiries or requests for permissions, please contact codwiz@yandex.ru.
+
+# AI-Generated Content Disclaimer
+# ---------------------------------
+# The content provided here has been generated using artificial intelligence (AI) technologies. 
+# Please note the following:
+
+# 1. Accuracy and Reliability:
+# The information, data, and statements presented in this content may not be accurate or factual. 
+# All outputs are automatically generated and have not been manually verified.
+
+# 2. No Liability:
+# The authors, developers, and platforms associated with the creation of this content are not liable 
+# for any consequences arising from its use. The content is provided "as is" without warranties of any kind.
+
+# 3. Intended Use:
+# This material is intended for informational purposes only. For critical decisions, please consult 
+# verified sources or qualified professionals.
+
+# 4. Artificial Intelligence:
+# The content was generated without direct human intervention in the creation process, which may 
+# affect its quality, accuracy, or relevance.
+
+# We encourage you to critically evaluate the information and not treat it as definitively reliable.
+# ---------------------------------
+# ---------------------------------------------------------------------------------
+# Name: Z
+# Description: Z.
+# Author: @zelenogoloviy_m
+# ---------------------------------------------------------------------------------
+# meta developer: @zelenogoloviy_m
+# scope: Z
+# scope: Z 0.0.1
+# ---------------------------------------------------------------------------------
+#imports
+from .. import loader, utils
+#class
+@loader.tds
+class z(loader.Module):
+    """z"""
+
+    strings = {"name": "Z", "z": "Z"}
+    strings_ru = {"z": "Z"}
+
+    @loader.command(
+        ru_doc="Z",
+    )
+    #function
+    async def z(self, message):
+        """Z"""
+        #Z
+        await utils.answer(message, self.strings("z"))
+# added symbols: ! #WARNING: AI CONTENT
