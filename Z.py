93e26ac4f3276e916732ad668e9682953dc30d82
zelenogoloviy <zelenogoloviy@gmail.com>
2025-08-18 14:11:50 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -1,4 +1,4 @@
-# the source code was taken from software "H", available via link: https://mods.codwiz.life/H.py. original source code is subject to a proprietary license. which you can read at the link: https://github.com/C0dwiz/H.Modules?tab=readme-ov-file#-license
+# The source code was taken from software "H", available via link: https://mods.codwiz.life/H.py.
 # Proprietary License Agreement
 
 # Copyright (c) 2024-29 CodWiz
@@ -16,55 +16,28 @@
 # 5. By using the Software, you agree to be bound by the terms and conditions of this license.
 
 # For any inquiries or requests for permissions, please contact codwiz@yandex.ru.
-
-# AI-Generated Content Disclaimer
-# ---------------------------------
-# The content provided here has been generated using artificial intelligence (AI) technologies. 
-# Please note the following:
-
-# 1. Accuracy and Reliability:
-# The information, data, and statements presented in this content may not be accurate or factual. 
-# All outputs are automatically generated and have not been manually verified.
-
-# 2. No Liability:
-# The authors, developers, and platforms associated with the creation of this content are not liable 
-# for any consequences arising from its use. The content is provided "as is" without warranties of any kind.
-
-# 3. Intended Use:
-# This material is intended for informational purposes only. For critical decisions, please consult 
-# verified sources or qualified professionals.
-
-# 4. Artificial Intelligence:
-# The content was generated without direct human intervention in the creation process, which may 
-# affect its quality, accuracy, or relevance.
-
-# We encourage you to critically evaluate the information and not treat it as definitively reliable.
-# ---------------------------------
 # ---------------------------------------------------------------------------------
 # Name: Z
-# Description: Z.
+# Description: Z
 # Author: @zelenogoloviy_m
 # ---------------------------------------------------------------------------------
 # meta developer: @zelenogoloviy_m
-# scope: Z
-# scope: Z 0.0.1
-# ---------------------------------------------------------------------------------
+__version__ = (0, 2, 0)
 #imports
 from .. import loader, utils
+
 #class
 @loader.tds
 class z(loader.Module):
     """z"""
-
-    strings = {"name": "Z", "z": "Z"}
-    strings_ru = {"z": "Z"}
+    strings = {"name": "З", "z": "З"}
+    strings_en = {"name": "Z", "z": "Z"}
 
     @loader.command(
-        ru_doc="Z",
+        en_doc="Z",
     )
     #function
     async def z(self, message):
-        """Z"""
-        #Z
+        """З"""
+        #З
         await utils.answer(message, self.strings("z"))
-# added symbols: ! #WARNING: AI CONTENT
